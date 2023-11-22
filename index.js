9f4216455caf1abb525818b8fdee6341711705f6
dav15jr <dnwanderajroutlook.com>
2023-11-22 14:45:15 +0100

    Added new icons for todo list, change all references from shpping list to todo list

---
+++ index.js
@@ -0,0 +1,65 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+    databaseURL: "https://testbed-a58bf-default-rtdb.europe-west1.firebasedatabase.app/"
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const toDoListInDB = ref(database, "ToDoList")
+
+const inputFieldEl = document.getElementById("input-field")
+const addButtonEl = document.getElementById("add-button")
+const toDoListEl = document.getElementById("todo-list")
+
+addButtonEl.addEventListener("click", function() {
+    let inputValue = inputFieldEl.value
+    
+    push(toDoListInDB, inputValue)
+    
+    clearInputFieldEl()
+})
+
+onValue(toDoListInDB, function(snapshot) {
+    if (snapshot.exists()) {
+        let itemsArray = Object.entries(snapshot.val())
+    
+        clearToDoListEl()
+        
+        for (let i = 0; i < itemsArray.length; i++) {
+            let currentItem = itemsArray[i]
+            let currentItemID = currentItem[0]
+            let currentItemValue = currentItem[1]
+            
+            appendItemToToDoListEl(currentItem)
+        }    
+    } else {
+        toDoListEl.innerHTML = "No items here... yet"
+    }
+})
+
+function clearToDoListEl() {
+    toDoListEl.innerHTML = ""
+}
+
+function clearInputFieldEl() {
+    inputFieldEl.value = ""
+}
+
+function appendItemToToDoListEl(item) {
+    let itemID = item[0]
+    let itemValue = item[1]
+    
+    let newEl = document.createElement("li")
+    
+    newEl.textContent = itemValue
+    
+    newEl.addEventListener("click", function() {
+        let exactLocationOfItemInDB = ref(database, `ToDoList/${itemID}`)
+        
+        remove(exactLocationOfItemInDB)
+    })
+    
+    toDoListEl.append(newEl)
+}
